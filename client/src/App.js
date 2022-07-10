26142290d36c2df75e86792d27632974b0c07e48
Carolyn B <bmathematics@gmail.com>
2022-07-10 18:54:45 -0400

    complete adding packages and file structure. Added first component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 export default App;
