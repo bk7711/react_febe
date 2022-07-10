4dc79f0f5cf16c1c21bcf3611f205a36197d52c0
Carolyn B <bmathematics@gmail.com>
2022-07-10 11:16:41 -0400

    initial client and server side setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+function App() {
+  return <div>Hello World</div>;
+}
+export default App;
